c7aeeab527543c0790ae5b5168b385edd02a24c8
Jan Halfar <jan@bestbytes.de>
2011-06-21 21:55:56 +0200

    reformatted upload

--- htdocs/services/upload.php
+++ htdocs/services/upload.php
@@ -1,3 +1,7 @@
 <?php
 
-\Foomo\Services\RPC::serveClass(new \Foomo\Zugspitze\Services\Upload(), new \Foomo\Services\RPC\Serializer\AMF(), 'org.foomo.zugspitze.services.upload');
+\Foomo\Services\RPC::serveClass(
+	new \Foomo\Zugspitze\Services\Upload(),
+	new \Foomo\Services\RPC\Serializer\AMF(),
+	'org.foomo.zugspitze.services.upload'
+);
